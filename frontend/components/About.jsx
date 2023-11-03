76d6c73288b60f2520fa9a712f0b22947621e95f
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-03 10:30:45 +0530

    Create About.jsx

---
+++ frontend/components/About.jsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <div id='about' className='dimension bg-white p-24'>
+        <div className='color-[#000]'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut distinctio cupiditate nemo, facilis totam odio. Dolorum, atque iure ab id ullam nam fuga, perspiciatis nesciunt at veritatis, vel laboriosam minus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, quae fugiat fugit at labore beatae tenetur aliquam recusandae iusto ratione quisquam consequuntur ex in, natus quasi? Quisquam asperiores recusandae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dolore adipisci sint quas quisquam perferendis laborum nihil ipsa mollitia hic. Perspiciatis aut repellat reprehenderit velit? Sunt dolores deleniti adipisci animi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi autem in consectetur, quidem excepturi libero aperiam eveniet ad magnam nobis exercitationem labore, recusandae minima provident sequi, nostrum a impedit. Ad? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nostrum consequuntur voluptatibus, suscipit aliquid, aliquam voluptatum ab cum optio modi fuga magni corrupti. Repellat ipsam voluptatem sed consequatur, quasi eligendi.
+        </div>
+    </div>
+  )
+}
