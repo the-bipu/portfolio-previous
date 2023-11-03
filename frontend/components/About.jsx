b4786547330a830f415c3a41273f5bc7e36fa696
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-04 02:04:38 +0530

    Update About.jsx

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -1,10 +1,24 @@
+'use client';
+
 import Image from 'next/image'
+
+import Navbar from './Navbar';
+import { motion } from 'framer-motion';
+import { staggerContainer, slideIn, fadeIn } from '../utils/motion';
 
 export default function About() {
   return (
-    <div id='about' className='dimension bg-white p-24'>
-        <div className='color-[#000]'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut distinctio cupiditate nemo, facilis totam odio. Dolorum, atque iure ab id ullam nam fuga, perspiciatis nesciunt at veritatis, vel laboriosam minus! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui, quae fugiat fugit at labore beatae tenetur aliquam recusandae iusto ratione quisquam consequuntur ex in, natus quasi? Quisquam asperiores recusandae delectus. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, dolore adipisci sint quas quisquam perferendis laborum nihil ipsa mollitia hic. Perspiciatis aut repellat reprehenderit velit? Sunt dolores deleniti adipisci animi. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi autem in consectetur, quidem excepturi libero aperiam eveniet ad magnam nobis exercitationem labore, recusandae minima provident sequi, nostrum a impedit. Ad? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem nostrum consequuntur voluptatibus, suscipit aliquid, aliquam voluptatum ab cum optio modi fuga magni corrupti. Repellat ipsam voluptatem sed consequatur, quasi eligendi.
+    <div id='about' className='w-[100%] lg:h-[745px] h-[auto] flex flex-col items-center bg-[#ffffff] p-10'>
+        <Navbar />
+
+        <motion.div
+          variants={fadeIn('up', 'tween', 0.6, 1)}
+          className='color-[#fff] text-[30px] py-8'
+        >
+          I know that <span className='color-[#0009D5]'>Good Design</span> Means <span className='color-[#0009D5]'>Good Business</span>
+        </motion.div>
+        <div className=''>
+            
         </div>
     </div>
   )
