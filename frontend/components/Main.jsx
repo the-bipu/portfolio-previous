6ecaa998d4e0a0273f9c6f67b8edbcc1acbe0a17
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-04 03:53:28 +0530

    Update Main.jsx

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -3,10 +3,11 @@ import Image from 'next/image'
 
 import Navbar from '@/components/Navbar'
 import { motion } from 'framer-motion';
+import { staggerContainer, slideIn, fadeIn } from '../utils/motion';
 
 export default function Main() {
   return (
-    <div id='main' className='w-[100%] lg:h-[745px] h-[auto] flex flex-col items-center bg-[#D9D9D9] p-10'>
+    <div id='main' className='w-[100%] lg:h-[748px] h-[auto] flex flex-col items-center bg-[#D9D9D9] p-10'>
         <Navbar />
         
         <div className='flex lg:flex-row flex-col lg:gap-0 gap-10 items-center color-[#000] py-12 lg:px-16 px-0'>
@@ -15,17 +16,25 @@ export default function Main() {
           <img src="./Ellipse-5.png" alt="" className='w-[auto] h-[auto] z-0 absolute top-[125px] right-10 lg:block hidden' />
           <img src="./Ellipse-6.png" alt="" className='w-[auto] h-[auto] z-0 absolute top-[165px] left-0 lg:block hidden' />
 
-          <div className="normal-shadow text-[23px] lg:w-[55%] w-[100%] h-[auto] text-center lg:px-24 px-10 z-10">
+          <motion.div 
+            variants={fadeIn('right', 'tween', 0.2, 1)}
+            initial="hidden" whileInView="show"
+            className="normal-shadow text-[23px] lg:w-[55%] w-[100%] h-[auto] text-center lg:px-24 px-10 z-10"
+          >
             Hello 🍃 <br />
             I’m The-Bipu 🤖 and <br />
             I’m a Full-Stack Web Developer. 🖥️ <br /><br />
 
             An adept full-stack web developer, specializing in React and Next.js, crafts dynamic and user-friendly web applications with seamless front-end and robust back-end expertise. 
-          </div>
+          </motion.div>
           
-          <div className="profile lg:w-[50%] w-[100%] h-[auto] flex items-center justify-center">
+          <motion.div 
+            variants={fadeIn('left', 'tween', 0.2, 1)}
+            initial="hidden" whileInView="show"
+            className="profile lg:w-[50%] w-[100%] h-[auto] flex items-center justify-center"
+          >
             <img src="../profile.png" alt="" className='lg:w-[500px] w-[90%] h-[auto]' />
-          </div>
+          </motion.div>
         </div>
     </div>
   )
