b6ca1fc463c7a057d9b0edf59ac9fd89cd4c4248
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-04 03:53:44 +0530

    Update About.jsx

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -8,17 +8,17 @@ import { staggerContainer, slideIn, fadeIn } from '../utils/motion';
 
 export default function About() {
   return (
-    <div id='about' className='w-[100%] lg:h-[745px] h-[auto] flex flex-col items-center bg-[#ffffff] p-10'>
+    <div id='about' className='w-[100%] lg:h-[748px] h-[auto] flex flex-col items-center bg-[#ffffff] p-10'>
         <Navbar />
 
         <motion.div
           variants={fadeIn('up', 'tween', 0.6, 1)}
           className='color-[#fff] text-[30px] py-8'
         >
-          I know that <span className='color-[#0009D5]'>Good Design</span> Means <span className='color-[#0009D5]'>Good Business</span>
+          I know that <span className='text-[#0009D5]'>Good Design</span> Means <span className='text-[#0009D5]'>Good Business</span>
         </motion.div>
-        <div className=''>
-            
+        <div className='color-[#000]'>
+            Cards
         </div>
     </div>
   )
