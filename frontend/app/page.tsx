ca0b428edea3f9d9bc82ec41b6fe96fa2aaaa4f5
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-04 03:53:47 +0530

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image'
 
 import Main from '@/components/Main'
 import About from '@/components/About'
+import Work from '@/components/Work'
 
 export default function Home() {
   return (
     <main className='w-[100%] h-[auto] p-0'>
       <Main />
       <About />
+      <Work />
     </main>
   )
 }
