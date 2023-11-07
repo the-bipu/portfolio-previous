676cb0fec9ffd195ccf3d6946a241ded437184a4
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-07 22:06:47 +0530

    Update About.jsx

--- frontend/components/About.jsx
+++ frontend/components/About.jsx
@@ -2,7 +2,6 @@
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image'
 
-import Navbar from './Navbar';
 import { motion } from 'framer-motion';
 import { urlFor, client } from '../app/client';
 
@@ -18,10 +17,9 @@ export default function About() {
 
   return (
     <div id='about' className='w-[100%] lg:h-[748px] h-[auto] flex flex-col items-center bg-[#ffffff] p-10'>
-        <Navbar />
 
         <motion.div
-          className='color-[#fff] text-[30px] py-8'
+          className='color-[#fff] text-[30px] py-8 text-center'
         >
           I know that <span className='text-[#0009D5]'>Good Design</span> Means <span className='text-[#0009D5]'>Good Business</span>
         </motion.div>
