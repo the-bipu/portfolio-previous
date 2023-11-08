c81a15e96a8c3055cdd5de6c9726faabaac9889a
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-09 02:33:53 +0530

    Update Contact.jsx

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -1,9 +1,40 @@
 'use client'
-import React from 'react'
+import React, { useState } from 'react'
 
 import { motion } from 'framer-motion';
+import { client } from '../app/client';
 
 export default function Contact() {
+
+  const [formData, setFormData] = useState({name: '', email: '', message: ''});
+  const [isFormSubmitted, setIsFormSubmitted] = useState(false)
+  const [loading, setLoading] = useState(false);
+
+  const { name , email, message } = formData;
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({...formData, [name]: value});
+  }
+
+  const handleSubmit = () => {
+    setLoading(true);
+
+    const contact = {
+      _type: 'contact',
+      name: formData.name,
+      email: 'email',
+      message: message,
+    }
+
+    client.create(contact)
+      .then(() => {
+        setLoading(false);
+        setIsFormSubmitted(true);
+      })
+  }
+
   return (
     <div id='contact' className='w-[100%] lg:h-[748px] h-[auto] flex flex-col items-center bg-[#D9D9D9] p-10'>
         
@@ -13,18 +44,56 @@ export default function Contact() {
             Contact <span className='text-[#0009D5]'> Me</span>
         </motion.div>
 
-        <div className='flex lg:flex-row flex-col lg:gap-10 gap-3 mt-[2rem]'>
-            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-[0.6rem] rounded-lg'>
-                Email@gmail.com
-            </motion.div>
-            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-[0.6rem] rounded-lg'>
-                +91 12345 67890
-            </motion.div>
+        <div className='flex lg:flex-row flex-col items-center justify-center lg:gap-8 gap-0'>
+          <div className='app__footer-card'>
+            <img src="./email.png" alt="email" />
+            <a href="mailto:hello@gmail.com" className='p-text'>
+              Hello@gmail.com
+            </a>
+          </div>
+
+          <div className='app__footer-card'>
+            <img src="./mobile.png" alt="mobile" />
+            <a href="tel: +1 (123) 456-789" className='p-text'>
+            +1 (123) 456-789
+            </a>
+          </div>
         </div>
 
-        <div className='w-[100%] h-[400px]'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed sunt consequuntur dolore, quas corporis ipsa perferendis mollitia. Molestiae libero accusantium, ipsum perspiciatis eligendi repellat sequi modi voluptatibus tempore dolores?
-        </div>
+        {!isFormSubmitted ? 
+          <div className='w-[60%] flex flex-col items-center justify-center mx-[1rem] my-[2rem]'>
+
+            <div className='flex items-center justify-center form--div'>
+              <input type="text" className='p-text textarea-style' name='name' placeholder='Your Name' value={name} onChange={handleChangeInput} />
+            </div>
+
+            <div className='flex items-center justify-center form--div'>
+              <input type="text" className='p-text textarea-style' name='email' placeholder='Your Email' value={email} onChange={handleChangeInput} />
+            </div>
+
+            <div className='form--div'>
+              <textarea 
+                className='h-[170px] p-text textarea-style'
+                placeholder='Your Message'
+                value={message}
+                name="message"
+                onChange={handleChangeInput}
+              ></textarea>
+            </div>
+
+            <button
+              type='button'
+              className='p-text submit--button hover:bg-[#2430af]'
+              onClick={handleSubmit}
+            >{loading ? 'Sending' : 'Send Message'}</button>
+          </div>
+        : 
+          <div>
+            <h3 className='head-text'>
+              Thank you for getting in touch! 
+            </h3>
+          </div>
+        }
     </div>
   )
 }
