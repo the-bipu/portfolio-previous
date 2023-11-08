8049d16d6bbe283219fdf49b9205c6fd628ab305
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-08 23:41:24 +0530

    Update Contact.jsx

--- frontend/components/Contact.jsx
+++ frontend/components/Contact.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 
 export default function Contact() {
   return (
-    <div id='contact' className='w-[100%] h-[auto] flex flex-col items-center bg-[#D9D9D9] p-10'>
+    <div id='contact' className='w-[100%] lg:h-[748px] h-[auto] flex flex-col items-center bg-[#D9D9D9] p-10'>
         
         <motion.div
           className='color-[#fff] text-[30px] text-[#000] pt-8 pb-2 text-center'
@@ -13,13 +13,17 @@ export default function Contact() {
             Contact <span className='text-[#0009D5]'> Me</span>
         </motion.div>
 
-        <div className='flex flex-row gap-10 mt-[2rem]'>
-            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-3'>
-                Email
+        <div className='flex lg:flex-row flex-col lg:gap-10 gap-3 mt-[2rem]'>
+            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-[0.6rem] rounded-lg'>
+                Email@gmail.com
             </motion.div>
-            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-3'>
-                Contact
+            <motion.div className='flex items-center justify-center bg-red-300 px-10 py-[0.6rem] rounded-lg'>
+                +91 12345 67890
             </motion.div>
+        </div>
+
+        <div className='w-[100%] h-[400px]'>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam sed sunt consequuntur dolore, quas corporis ipsa perferendis mollitia. Molestiae libero accusantium, ipsum perspiciatis eligendi repellat sequi modi voluptatibus tempore dolores?
         </div>
     </div>
   )
