ccd28e14ee4c5b74acbbee36ba37a0bbb6dd95b8
the-bipu <127449591+the-bipu@users.noreply.github.com>
2023-11-09 18:09:09 +0530

    Update client.js

--- frontend/app/client.js
+++ frontend/app/client.js
@@ -2,11 +2,11 @@ import { createClient } from '@sanity/client';
 import imgUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: process.env.PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: process.env.TOKEN,
 });
 
 const builder = imgUrlBuilder(client);
